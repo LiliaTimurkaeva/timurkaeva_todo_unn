b84eff7d89741793d6e049ae6236a42c0ed4b39e
Lilia Timurkaeva <lilytimurluck@gmail.com>
2020-04-22 15:13:06 +0300

    Инсталяция джанго

---
+++ timurkaeva_todo_unn/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for timurkaeva_todo_unn project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timurkaeva_todo_unn.settings')
+
+application = get_wsgi_application()
